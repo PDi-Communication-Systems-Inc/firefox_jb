03078aa8cff9ee3df8e6fcc3b61a87b22c7a74cc
Meera Gangakhedkar <mgangakhedkar@pdiarm.com>
2018-12-11 15:31:40 -0500

    Firefox apk and Make files for JB NIH solo

---
+++ Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := shared
+
+# Module name should match apk name to be installed.
+LOCAL_MODULE := firefox_63
+#LOCAL_MODULE := AdobeReader
+LOCAL_REQUIRED_MODULES := libmozglue.so
+LOCAL_REQUIRED_MODULES += libplugin-container.so
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE :=  libmozeglue.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH  := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SRC_FILES := lib/armeabi-v7a/$(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE :=  libplugin-container.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH  := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SRC_FILES := lib/armeabi-v7a/$(LOCAL_MODULE)
+
+include $(BUILD_PREBUILT)
